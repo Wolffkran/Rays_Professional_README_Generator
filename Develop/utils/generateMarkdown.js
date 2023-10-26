77203d48c509e5f913f705914138f0c0cc88ea9c
Wolffkran <liebherr2008@live.com>
2023-10-26 06:05:31 -0400

    Added function to return a license badge based on the license passed in, returns empty string if no license

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,14 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to return a license badge based on the license passed in 
+function renderLicenseBadge(license) {
+  if (license) {
+    if (license === 'MIT') {
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    } else if (license === 'Apache 2.0') {
+      return '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    }
+  }
+  return ''; // Return an empty string if there's no license
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
